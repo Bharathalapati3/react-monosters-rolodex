d77f87bd6869d2c23cbffc344f74efc5964330b7
Bharathalapati3 <bharathalapati03@gmail.com>
2019-08-10 10:43:50 +0530

    Updates

---
+++ precache-manifest.4106f75a00783e0d6a38d0691fc2f09b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637152c50e5ae482426c8c512a32ceea",
+    "url": "/react-monosters-rolodex/index.html"
+  },
+  {
+    "revision": "1546d48373c1d8bec1d3",
+    "url": "/react-monosters-rolodex/static/css/main.7496467e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react-monosters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "1546d48373c1d8bec1d3",
+    "url": "/react-monosters-rolodex/static/js/main.466215c2.chunk.js"
+  },
+  {
+    "revision": "82f8f98c2daae4095f35",
+    "url": "/react-monosters-rolodex/static/js/runtime~main.bcfc6b9f.js"
+  }
+]);
